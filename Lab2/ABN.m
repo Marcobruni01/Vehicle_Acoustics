b24a3fbfc201b4d941a3ca0f1482148f56f2cc36
MattiaDallaCosta <matty7787878@gmail.com>
2024-12-15 06:16:22 +0100

    started work on LAB2/ABN

---
+++ Lab2/ABN.m
@@ -0,0 +1,11 @@
+clc
+clear
+close all
+%%
+
+test1 = readtable('students_data/ABN/Test1.txt');
+test2 = readtable('students_data/ABN/Test2.txt');
+test3 = readtable('students_data/ABN/Test3.txt');
+test4 = readtable('students_data/ABN/Test4.txt');
+
+%%
